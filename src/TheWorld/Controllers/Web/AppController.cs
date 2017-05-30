38db3cec15b6cb87e16524296445395f89c1fc5d
Matt Holland <mattholland0202@gmail.com>
2017-05-30 18:02:19 +0100

    8.5

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using TheWorld.Models;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+        private IWorldRepository _repository;
+        private ILogger<AppController> _logger;
+
+        public AppController(IMailService mailService, IConfigurationRoot config, IWorldRepository repository, ILogger<AppController> logger)
+        {
+            _mailService = mailService;
+            _config = config;
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var data = _repository.GetAllTrips();
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get Trips in Index page: {ex.Message}");
+                return Redirect("/error");
+            }
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains(".ac.uk"))
+            {
+                ModelState.AddModelError("Email", "We don't support .ac.uk domains");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "Test Message", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿/* Put all JS code in an immediately invoked function expression (AKA self-executing anonymous function) so that variables etc aren't added to global scope */
+(function () {
+    //var ele = $("#username");
+    //ele.text("MyNewName");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style.background = "";
+    //});
+
+    //var menuItems = $("ul.menu li a"); // The anchor inside a listItem inside a menu for an unordered list
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); // returns a "wrapped set" of DOM elements
+    var $icon = $("#sidebarToggle i.fa"); // the "i" is a child of it, classed with fa
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+        }
+    });
+    
+})();
