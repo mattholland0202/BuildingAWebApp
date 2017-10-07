eb646df4057ae8503c2547b2eea9ab3b8d5e26a3
Matt Holland <mattholland0202@gmail.com>
2017-10-07 12:07:05 +0100

    10.7

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -4,23 +4,45 @@
     angular.module("app-trips")     // Calling this with only one param means get the existing module
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        },
-        {
-            name: "World Trip",
-            created: new Date()
-        }]
+        //vm.trips = [{
+        //    name: "US Trip",
+        //    created: new Date()
+        //},
+        //{
+        //    name: "World Trip",
+        //    created: new Date()
+        //}]
+
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips") // returns a promise
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
         vm.addNewTrip = function () {
-            alert(vm.newTrip.name);
+            vm.trips.push(
+                {
+                    name: vm.newTrip.name,
+                    created: new Date()
+                });
+            vm.newTrip = {};
         };
-    }
+    };
 })();
